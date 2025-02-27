fed36e61625dc10f97da8b9fbd2d93c58342d4aa
AnhadWander <anhad.wander@ucalgary.ca>
2025-02-27 15:57:43 -0700

    Update Location.java

--- edu/ucalgary/oop/Location.java
+++ edu/ucalgary/oop/Location.java
@@ -1,32 +1,31 @@
-package edu.calgary.oop;
+package edu.ucalgary.oop;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class Location {
-    private String name;
-    private String address;
-    private List<DisasterVictim> occupants;
-    private List<Supply> supplies;
+	private String name;
+	private String address;
+	private ArrayList<DisasterVictim> occupants;
+	private ArrayList<Supply> supplies;
 
-    public Location(String name, String address) {
-        this.name = name;
-        this.address = address;
-        this.occupants = new ArrayList<>();
-        this.supplies = new ArrayList<>();
-    }
+	public Location(String name, String address) {
+		this.name = name;
+		this.address = address;
+		this.occupants = new ArrayList<DisasterVictim>();
+		this.supplies = new ArrayList<Supply>();
+	}
 
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-    public String getAddress() { return address; }
-    public void setAddress(String address) { this.address = address; }
-    public List<DisasterVictim> getOccupants() { return occupants; }
-    public void setOccupants(List<DisasterVictim> occupants) { this.occupants = occupants; }
-    public List<Supply> getSupplies() { return supplies; }
-    public void setSupplies(List<Supply> supplies) { this.supplies = supplies; }
-    
-    public void addOccupant(DisasterVictim occupant) { occupants.add(occupant); }
-    public void removeOccupant(DisasterVictim occupant) { occupants.remove(occupant); }
-    public void addSupply(Supply supply) { supplies.add(supply); }
-    public void removeSupply(Supply supply) { supplies.remove(supply); }
+	public String getName() { return this.name; }
+	public String getAddress() { return this.address; }
+	public ArrayList<DisasterVictim> getOccupants() { return this.occupants; }
+	public ArrayList<Supply> getSupplies() { return this.supplies; }
+	public void setName(String name) { this.name = name; }
+	public void setAddress(String address) { this.address = address; }
+	public void setOccupants(ArrayList<DisasterVictim> occupants) { this.occupants = occupants; }
+	public void addOccupant(DisasterVictim occupant) { this.occupants.add(occupant); }
+
+	public void removeOccupant(DisasterVictim occupant) { this.occupants.remove(occupant); }
+	public void setSupplies(ArrayList<Supply> supplies) { this.supplies = supplies; }
+	public void addSupply(Supply supply) { this.supplies.add(supply); }
+	public void removeSupply(Supply supply) { this.supplies.remove(supply); }
 }
