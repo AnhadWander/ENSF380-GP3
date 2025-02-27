c44ae6835c01dfeb34860180534d7ed35bf436c0
AnhadWander <anhad.wander@ucalgary.ca>
2025-02-27 15:57:22 -0700

    Update Inquirer.java

--- edu/ucalgary/oop/Inquirer.java
+++ edu/ucalgary/oop/Inquirer.java
@@ -1,23 +1,23 @@
-package edu.calgary.oop;
+package edu.ucalgary.oop;
 
 import java.util.ArrayList;
 import java.util.List;
 
 class Inquirer {
-    private String firstName;
-    private String lastName;
-    private String servicesPhone;
-    private String info;
+	private String firstName;
+	private String lastName;
+	private String servicesPhone;
+	private String info;
 
-    public Inquirer(String firstName, String lastName, String phone, String info) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.servicesPhone = phone;
-        this.info = info;
-    }
+	public Inquirer(String firstName, String lastName, String phone, String info) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.servicesPhone = phone;
+		this.info = info;
+	}
 
-    public String getFirstName() { return firstName; }
-    public String getLastName() { return lastName; }
-    public String getServicesPhoneNum() { return servicesPhone; }
-    public String getInfo() { return info; }
+	public String getFirstName() { return firstName; }
+	public String getLastName() { return lastName; }
+	public String getServicesPhoneNum() { return servicesPhone; }
+	public String getInfo() { return info; }
 }
