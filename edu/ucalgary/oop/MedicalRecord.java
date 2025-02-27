e1e41e3c470053695d27b9bd5c1a681f1b7fe023
AnhadWander <anhad.wander@ucalgary.ca>
2025-02-27 15:58:21 -0700

    Update MedicalRecord.java

--- edu/ucalgary/oop/MedicalRecord.java
+++ edu/ucalgary/oop/MedicalRecord.java
@@ -1,50 +1,49 @@
 package edu.ucalgary.oop;
 
 public class MedicalRecord {
+	private Location location;
+	private String treatmentDetails;
+	private String dateOfTreatment;
 
-    private Location location;
-    private String treatmentDetails;
-    private String dateOfTreatment;
+	public MedicalRecord(Location location, String treatmentDetails, String dateOfTreatment) {
+		this.location = location;
+		this.treatmentDetails = treatmentDetails;
+		this.dateOfTreatment = dateOfTreatment;
+	}
 
-    public MedicalRecord(Location location, String treatmentDetails, String dateOfTreatment) {
-        this.location = location;
-        this.treatmentDetails = treatmentDetails;
-        this.dateOfTreatment = dateOfTreatment;
-    }
+	public Location getLocation() {
+		return location;
+	}
 
-    public Location getLocation() {
-        return location;
-    }
+	public void setLocation(Location location) {
+		this.location = location;
+	}
 
-    public void setLocation(Location location) {
-        this.location = location;
-    }
+	public String getTreatmentDetails() {
+		return treatmentDetails;
+	}
 
-    public String getTreatmentDetails() {
-        return treatmentDetails;
-    }
+	public void setTreatmentDetails(String treatmentDetails) {
+		this.treatmentDetails = treatmentDetails;
+	}
 
-    public void setTreatmentDetails(String treatmentDetails) {
-        this.treatmentDetails = treatmentDetails;
-    }
+	public String getDateOfTreatment() {
+		return dateOfTreatment;
+	}
 
-    public String getDateOfTreatment() {
-        return dateOfTreatment;
-    }
+	public void setDateOfTreatment(String dateOfTreatment) {
+		isValidDateFormat(dateOfTreatment);
+		this.dateOfTreatment = dateOfTreatment;
+	}
 
-    public void setDateOfTreatment(String dateOfTreatment) {
-        isValidDateFormat(dateOfTreatment);
-        this.dateOfTreatment = dateOfTreatment;
-    }
-
-    private String isValidDateFormat(String Date) {
-        String[] date = Date.split("-");
-        if (date.length != 3) {
-            throw new IllegalArgumentException("Invalid date format");
-        }
-        if (date[0].length() != 4 || date[1].length() != 2 || date[2].length() != 2) {
-            throw new IllegalArgumentException("Invalid date format");
-        }
-        return "Valid date format";
-    }
+	private String isValidDateFormat(String Date) {
+		String[] date = Date.split("-");
+		if (date.length != 3) {
+			throw new IllegalArgumentException("Invalid date format");
+		}
+		if (date[0].length() != 4 || date[1].length() != 2 || date[2].length() != 2) {
+			throw new IllegalArgumentException("Invalid date format");
+		}
+		return "Valid date format";
+	}
 }
